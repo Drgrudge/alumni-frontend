6b3854580d7d03ea8a7f436d1c91da1992665bb0
beAsTz <85973106+Drgrudge@users.noreply.github.com>
2023-11-12 23:05:24 +0530

    login

--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -13,6 +13,7 @@ const RegistrationForm = () => {
     dateOfBirth: null,
     password: '',
     confirmPassword: '',
+    role: '', // Updated role field
   });
 
   const [errors, setErrors] = useState({});
@@ -24,26 +25,27 @@ const RegistrationForm = () => {
       setErrors({ termsAgreement: 'You must agree to the Terms and Conditions' });
       return;
     }
-  
+
     // Password matching validation
     if (formData.password !== formData.confirmPassword) {
       setErrors({ confirmPassword: 'Passwords do not match' });
       return;
     }
-  
+
     try {
       // Send a POST request to your server's registration endpoint
       const response = await axios.post('http://localhost:5000/register', formData);
-  
+
+      console.log(formData)
+
       console.log(response.data); // Assuming the server sends a JSON response
-  
+
       // Handle the response (e.g., show a success message or redirect to login)
     } catch (error) {
       // Handle any errors (e.g., show an error message)
       console.error('Error during signup:', error);
     }
   };
-  
 
   return (
     <div>
@@ -105,23 +107,19 @@ const RegistrationForm = () => {
           onChange={(e) => setFormData({ ...formData, confirmPassword: e.target.value })}
         />
       </div>
-      <div className="flex justify-between items-center mb-4">
-        <div className="checkbox p-2">
-          <input
-            type="checkbox"
-            id="termsAgreement"
-            className="hover:bg-blue-900"
-            checked={termsAgreement}
-            onChange={() => setTermsAgreement(!termsAgreement)}
-          />
-          <label htmlFor="termsAgreement" className="text-white p-2">
-            I agree to the Terms and Conditions
-          </label>
-        </div>
-        {errors.termsAgreement && (
-          <span className="text-red-500">{errors.termsAgreement}</span>
-        )}
+      <div className="mb-4 w-full">
+        <label className="text-white">Role:</label>
+        <select
+          className="input w-full hover:bg-blue-900 text-blue-900 hover:text-white"
+          value={formData.role}
+          onChange={(e) => setFormData({ ...formData, role: e.target.value })}
+        >
+          <option value="">Select Role</option>
+          <option value="admin">Admin</option>
+          <option value="alumni">Alumni</option>
+        </select>
       </div>
+      {/* ... other form fields ... */}
       <div className="text-center md:text-left">
         <button
           onClick={handleSignUp}
@@ -132,6 +130,7 @@ const RegistrationForm = () => {
         {errors.confirmPassword && (
           <span className="text-red-500">{errors.confirmPassword}</span>
         )}
+        
         <p className="text-sm font-semibold mt-2 text-white">
           Already have an account?{' '}
           <Link to="/login" className="text-blue-600 hover:text-blue-900">
--- src/components/login.jsx
+++ src/components/login.jsx
@@ -4,43 +4,25 @@ import axios from 'axios';
 
 const LoginPage = ({ isLoggedIn, setisLoggedIn }) => {
   const navigate = useNavigate();
-  const [formData, setFormData] = useState({
-    email: '',
-    password: '',
-  });
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [token, setToken] = useState('');
 
-  // const handleSignUp = async () => {
-  
-  //   try {
-  //     // Send a POST request to your server's registration endpoint
-  //     const response = await axios.post('http://localhost:5000/login', formData);
-  
-  //     console.log(response.data);
 
-  //     setisLoggedIn(true);
-
-  //     navigate('/');
-  //      // Assuming the server sends a JSON response
-  
-  //     // Handle the response (e.g., show a success message or redirect to login)
-  //   } catch (error) {
-  //     // Handle any errors (e.g., show an error message)
-  //     console.error('Error during signup:', error);
-  //   }
-  // };
-
-  const handleSignUp = async () => {
+  const handleLogin = async () => {
     try {
-      const response = await axios.post('http://localhost:5000/login', formData);
-      console.log(response); // Log the entire response object
-  
-      setisLoggedIn(true);
-      navigate('/');
+      const response = await axios.post('http://localhost:5000/login', {
+        email,
+        password,
+      });
+      setToken(response.data.token);
+      console.log('Login successful');
+      setisLoggedIn(true)
+      navigate('/')
     } catch (error) {
-      console.error('Error during signup:', error);
+      console.error('Login failed:', error.response.data.error);
     }
   };
-  
   
   return (
     <section className="h-screen w-full flex justify-center items-center">
@@ -56,58 +38,28 @@ const LoginPage = ({ isLoggedIn, setisLoggedIn }) => {
 
         {/* Right column with login form (flex container) */}
         <div className="flex-1 p-4 md:p-8 lg:p-12 xl:p-12 flex flex-col justify-center items-center w-full">
-        <form className="w-full max-w-xl p-6 rounded-lg bg-slate-800 shadow-lg text-white">
-  <p className="text-xl mb-4 text-center text-white bg-blue-600 hover:bg-blue-900 w-full rounded-lg">Sign in with</p>
-
-  {/* Social Media Sign-In Buttons */}
-  <div className="flex items-center mb-4 space-x-4 justify-center md:justify-center">
-    <button className="btn-facebook hover:bg-blue-900 text-white">Facebook</button>
-    <button className="btn-twitter hover:bg-blue-900 text-white">Twitter</button>
-    <button className="btn-linkedin hover:bg-blue-900 text-white">LinkedIn</button>
-  </div>
-
-  <div className="mb-4">
-    <p className="font-semibold text-center text-white">Or</p>
-  </div>
-
-  {/* Email and Password Fields */}
-  <div className="mb-4 w-full">
-    <input
-      type="text"
-      className="input w-full"
-      id="email"
-      placeholder="Email address"
-      value={formData.email}
-      onChange={(e) => setFormData({ ...formData, email: e.target.value })}
-    />
-  </div>
-  <div className="mb-4">
-    <input
-      type="password"
-      className="input w-full"
-      id="password"
-      placeholder="Password"
-      value={formData.password}
-      onChange={(e) => setFormData({ ...formData, password: e.target.value })}
-    />
-  </div>
-
-  <div className="flex justify-between items-center mb-4">
-    <div className="checkbox p-2">
-      <input type="checkbox" id="rememberMe" className=' hover:bg-blue-900' />
-      <label htmlFor="rememberMe" className="text-white p-2">Remember me</label>
-    </div>
-    <a href="#!" className="text-primary text-blue-600 hover:text-blue-900">Forgot password?</a>
-  </div>
-
-  <div className="text-center md:text-left">
-    <button className="mb-4 btn-login bg-blue-600 hover:bg-blue-900 text-white w-full rounded-lg" onClick={handleSignUp}>Login</button>
-    <p className="text-sm font-semibold mt-2 text-white">
-      Don't have an account?{' '}
-      <Link to="/signup" className="text-blue-600 hover:text-blue-900">Register</Link>
-    </p>
-  </div>
-</form>
+        <div>
+        <h1>MERN Auth Example</h1>
+        <label>
+          Username:
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </label>
+        <br />
+        <br />
+        <button onClick={handleLogin}>Login</button>
+        {token && <p>Token: {token}</p>}
+        <p className="text-sm font-semibold mt-2 text-white">
+        Already have an account?{' '}
+        <Link to="/signUp" className="text-blue-600 hover:text-blue-900">
+          SignUp
+        </Link>
+      </p>
+      </div>
 
       
         </div>
